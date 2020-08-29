bab4b36a55230402efcaeb1a9de86d44cc6dcdda
addyxvii <addisonbenet@gmail.com>
2020-08-29 02:22:45 -0600

    passing test woohoo

--- test/Token.test.js
+++ test/Token.test.js
@@ -2,13 +2,14 @@ const Token = artifacts.require('./Token');
 
 require('chai')
     .use(require('chai-as-promised'))
+    .should()
+
 contract('Token', (accounts) => {
 
     describe('deployment', () => {
         it('tracks the name', async () => {
            const token = await Token.new()
            const result = await token.name()
-           console.log(token.name)
            result.should.equal('botany')
         })
     })
