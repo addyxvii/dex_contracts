5c1b1a164d45f1a22cbc7fd416ccdd21bf76c7df
addyxvii <addisonbennett@gmail.com>
2020-08-31 20:39:03 -0600

    remove white space

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,14 +3,16 @@ pragma solidity ^0.5.0;
 
 contract Token {
     string public name = "botany";
-    string public symbol = "BOT";  
+    string public symbol = "BOT";
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
     //Track Balance
+    mapping(address => uint256) public balanceOf;
     //Send Tokens
 
     constructor() public {
         totalSupply = 1000000 * (10 ** decimals);
+        balanceOf[msg.sender] = totalSupply;
     }
 }
