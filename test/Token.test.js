1e024ca77499c7d039d327086b14453c04122bfe
addyxvii <addisonbenet@gmail.com>
2020-08-31 20:24:58 -0600

    U+1F928

--- test/Token.test.js
+++ test/Token.test.js
@@ -1,17 +1,44 @@
-const Token = artifacts.require('./Token');
+const Token = artifacts.require('./Token')
 
 require('chai')
-    .use(require('chai-as-promised'))
-    .should()
+  .use(require('chai-as-promised'))
+  .should()
 
 contract('Token', (accounts) => {
+  const name = 'botany'
+  const symbol = 'BOT'
+  const decimals = '18'
+  const totalSupply = '1000000000000000000000000'
+  let token
 
-    describe('deployment', () => {
-        it('tracks the name', async () => {
-           const token = await Token.new();
-           const result = await token.name();
-           
-           result.should.equal('botany')
-        })
-    });
-});
+  beforeEach(async () => {
+    token = await Token.new()
+  })
+
+  describe('deployment', () => {
+    it('tracks the name', async () => {
+      const result = await token.name()
+      result.should.equal(name)
+    })
+
+    it('tracks the symbol', async ()  => {
+      const result = await token.symbol()
+      result.should.equal(symbol)
+    })
+
+    it('tracks the decimals', async ()  => {
+      const result = await token.decimals()
+      result.toString().should.equal(decimals)
+    })
+
+    it('tracks the total supply', async ()  => {
+      const result = await token.totalSupply()
+      result.toString().should.equal(totalSupply)
+    })
+
+    it('assings the total supply to the supplier', async () => {
+        const result = await token.balanceOf(accounts[0])
+        result.toString().shouldEqual(tokens(totalSupply))
+    })
+  })
+})
