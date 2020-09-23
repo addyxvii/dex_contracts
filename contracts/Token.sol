22aae556aa46fdec3fb4d4989d5ba73bc5cbffdd
addyxvii <addisonbennett@gmail.com>
2020-09-23 16:23:00 -0600

    added erc20 variables

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,5 +2,9 @@ pragma solidity ^0.5.0;
 
 
 contract Token {
-    string public name = 'botany'; 
+    string public name = "botany";
+    string public symbol = "BOT";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
 }
